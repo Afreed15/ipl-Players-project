d7a277fa62cba1853207cbae092ed2bd2aa1eb92
Afreed15 <Afreed.vs786@gmail.com>
2021-03-16 14:14:40 +0530

    players ipl

---
+++ Controllers/IPLController.cs
@@ -0,0 +1,32 @@
+﻿using ProjectCheck.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectCheck.Controllers
+{
+    public class IPLController : Controller
+    {
+        
+        public ActionResult Teams()
+        {
+            IPLCheckEntities db = new IPLCheckEntities();
+
+            return View(db.Teams.ToList());
+        }
+        public ActionResult LPlayers(int id)
+        {
+            IPLCheckEntities db = new IPLCheckEntities();
+            List<Player> Playerlist = db.Players.Where(x => x.Tid == id).ToList();
+            return View(Playerlist);
+        }
+        public ActionResult PDetails(int id)
+        {
+            IPLCheckEntities db = new IPLCheckEntities();
+            List<PProfile> Playerlist = db.PProfiles.Where(x => x.Pid == id).ToList();
+            return View(Playerlist);
+        }
+    }
+}
---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,19 @@
+﻿using ProjectCheck.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectCheck.Controllers
+{
+    public class PlayersController : Controller
+    {
+        // GET: Players
+        public ActionResult Index()
+        {
+            IPLCheckEntities db = new IPLCheckEntities();
+            return View(db.PProfiles.ToList());
+        }
+    }
+}
